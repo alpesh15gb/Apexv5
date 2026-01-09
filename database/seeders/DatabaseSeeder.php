eec268a202e6f7bcf0ee6dcc2e3351aa8de280e7
Your Name <you@example.com>
2026-01-09 22:24:54 +0530

    Add User Migration, Model and Admin Seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Check if admin exists
+        if (!User::where('email', 'admin@apex.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@apex.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+    }
+}
