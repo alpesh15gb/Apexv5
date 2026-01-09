c2e27d225b83b09753afdbf2c5281628768e8635
Your Name <you@example.com>
2026-01-10 01:31:10 +0530

    Remove encryption settings - let Driver 17 use defaults

--- config/database.php
+++ config/database.php
@@ -39,8 +39,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'encrypt' => 'no',
-            'trust_server_certificate' => 'true',
         ],
 
     ],
